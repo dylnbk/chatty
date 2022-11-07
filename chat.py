4c379146f16c41616db68d4afdb0c12922f76c4a
dylnbk <dyln.bk@gmail.com>
2022-11-07 23:48:19 +0000

    Stateful update

--- chat.py
+++ chat.py
@@ -9,7 +9,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.8, top_p=1, tokens=400, freq_pen=0.0, pres_pen=0.2, stop=['BBR:', 'You:']):
+def gpt3_completion(prompt, engine='text-davinci-002', temp=0.8, top_p=1, tokens=1000, freq_pen=0.0, pres_pen=0.0, stop=['You:']):
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
     response = openai.Completion.create(
         engine=engine,
@@ -23,19 +23,30 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.8, top_p=1, tokens
     text = response['choices'][0]['text'].strip()
     return text
 
+st.set_page_config(
+    page_title="chatty",
+    page_icon="💬",
+    menu_items={
+        'Report a bug': "mailto:dyln.bk@gmail.com",
+        'About': "Chat to the AI!"
+    }
+)
+
 local_css("style.css")
 openai.api_key = st.secrets["openaiapikey"]
 
-if __name__ == '__main__':
-    conversation = []                                        
+if 'conversation' not in st.session_state:
+    st.session_state.conversation = []
+
+if __name__ == '__main__':                                       
     form = st.form("input", clear_on_submit=True)
     user_input = form.text_area('Input', label_visibility="hidden")
     form.form_submit_button("Send")
     st.markdown('***')
-    conversation.append(f'You: {user_input}')
-    text_block = '\n'.join(conversation)
+    st.session_state.conversation.append(f'You: {user_input}')
+    text_block = '\n'.join(st.session_state.conversation)
     prompt = open_file('promptchat.txt').replace('<<BLOCK>>', text_block)
-    prompt = prompt + '\nBBR: '
+    prompt = prompt + '\nMotoko: '
     response = gpt3_completion(prompt)
     st.write(f'{response}')
-    conversation.append(f'BBR: {response}')
+    st.session_state.conversation.append(f'Motoko: {response}')
