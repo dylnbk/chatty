bd821bd1b9d415718c26a1b0070f4ca21dca5bcc
dylnbk <dyln.bk@gmail.com>
2022-11-08 01:05:00 +0000

    Stateful fix

--- chat.py
+++ chat.py
@@ -9,6 +9,9 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+def check_true():
+    st.session_state.check = True
+
 def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1, tokens=1000, freq_pen=0.0, pres_pen=0.0, stop=['Motoko:', 'You:']):
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
     response = openai.Completion.create(
@@ -28,7 +31,8 @@ st.set_page_config(
     page_icon="💬",
     menu_items={
         'Report a bug': "mailto:dyln.bk@gmail.com",
-        'About': "Chat to the AI!"
+        'Get help': None,
+        'About': "Made by dyln.bk - Chat with GPT3!"
     }
 )
 
@@ -38,15 +42,19 @@ openai.api_key = st.secrets["openaiapikey"]
 if 'conversation' not in st.session_state:
     st.session_state.conversation = []
 
+if 'check' not in st.session_state:
+    st.session_state.check = False
+
 if __name__ == '__main__':                                       
     form = st.form("input", clear_on_submit=True)
     user_input = form.text_area('Input', label_visibility="hidden")
-    form.form_submit_button("Send")
+    form.form_submit_button("Send", on_click=check_true)
     st.markdown('***')
-    st.session_state.conversation.append(f'You: {user_input}') #
-    text_block = '\n\n\n'.join(st.session_state.conversation)
-    prompt = open_file('promptchat.txt').replace('<<BLOCK>>', text_block)
-    prompt = prompt + '\n\nMotoko: '
-    response = gpt3_completion(prompt)
-    st.session_state.conversation.append(f'Motoko: {response}')
-    st.write(f'{response}')
+    if st.session_state.check:
+        st.session_state.conversation.append(f'You: {user_input}') #
+        text_block = '\n\n\n'.join(st.session_state.conversation)
+        prompt = open_file('promptchat.txt').replace('<<BLOCK>>', text_block)
+        prompt = prompt + '\n\nMotoko: '
+        response = gpt3_completion(prompt)
+        st.session_state.conversation.append(f'Motoko: {response}')
+        st.write(f'{response}')
