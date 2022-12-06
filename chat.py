73046a282db58e730dfec6f55c01a4d371ae8324
dylnbk <dyln.bk@gmail.com>
2022-12-06 21:12:37 +0000

    Update char limit, remove padding

--- chat.py
+++ chat.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
         with st.form("input_motoko", clear_on_submit=True):     
 
             # text area for user input limited to 1k chars
-            user_input = st.text_area('Enter a message:', max_chars=250)
+            user_input = st.text_area('Enter a message:', max_chars=500)
 
             # submit button with onclick that udpates session state 
             st.form_submit_button("Submit", on_click=check_true_motoko)
@@ -190,7 +190,7 @@ if __name__ == '__main__':
         with st.form("input_explain", clear_on_submit=True):   
 
             # text area for user input limited to 1k chars
-            user_input = st.text_area('Enter a message:', max_chars=1000)
+            user_input = st.text_area('Enter a message:', max_chars=1250)
 
             # submit button with onclick that udpates session state 
             st.form_submit_button("Submit", on_click=check_true_explain)
